c52c3f59bb69ce9746defe22c851fef443da7564
Francois Janson <jansonfrancois@gmail.com>
2021-07-08 15:05:54 +0200

    Exercise without Bootstrap

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function showMenu() {
+    var linksBlock = document.getElementById("navlinks");
+    
+    if (linksBlock.style.display == "none") {
+        linksBlock.style.display = "block";
+    }
+    else {
+        linksBlock.style.display = "none"
+    }
+}
